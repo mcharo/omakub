54150323de9a8dc0def45d9f30fd3612bb4e4f6d
Michael Charo <michael@opstack.com>
2025-07-04 17:02:57 -0500

    LocalSend has arm64 releases

--- install/desktop/app-localsend.sh
+++ install/desktop/app-localsend.sh
@@ -1,6 +1,14 @@
 cd /tmp
 LOCALSEND_VERSION=$(curl -s "https://api.github.com/repos/localsend/localsend/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/LocalSend-${LOCALSEND_VERSION}-linux-x86-64.deb"
+
+ARCHITECTURE=$(dpkg --print-architecture)
+if [ "$ARCHITECTURE" = "arm64" ]; then
+    DEB_FILE="LocalSend-${LOCALSEND_VERSION}-linux-arm64.deb"
+else
+    DEB_FILE="LocalSend-${LOCALSEND_VERSION}-linux-x86-64.deb"
+fi
+
+wget -O localsend.deb "https://github.com/localsend/localsend/releases/latest/download/$DEB_FILE"
 sudo apt install -y ./localsend.deb
 rm localsend.deb
 cd -
